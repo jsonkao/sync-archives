7da91dd96aa0cec2f353ddd71c233d85cb5fc8c7
Jason Kao <jason.kao@columbia.edu>
2020-04-02 16:29:04 -0400

    upload script with concurrency

--- upload.js
+++ upload.js
@@ -1,25 +1,37 @@
 const fs = require('fs').promises;
 const axios = require('axios');
+const chalk = require('chalk');
+const pLimit = require('p-limit');
 require('dotenv').config();
 
+const { ROOT } = require('./constants.js');
 const headers = { 'Authorization': 'token ' + process.env.TOKEN };
 
-const CONTENT_PATH = 'https://api.github.com/repos/jsonkao/lionshare-scraper/contents/archive-postings/columbia';
-
 // Create a file
-function putFile(fname) {
-  return fs.readFile(fname, { encoding: 'utf-8' })
-    .then(contents => axios.request({
-      url: CONTENT_PATH,
-      data: { content: contents },
-      headers,
-      maxBodyLength: Infinity,
-      maxContentLength: Infinity,
-    }))
-    .then(response => console.log(response))
-    .catch(e => {
-      Object.keys(e.config).forEach(k => k !== 'data' && console.log(k, e.config[k]))
+function putFile(origin, destination) {
+  return fs.readFile(origin)
+    .then(contents => {
+      console.log('Uploading', origin);
+      return axios.request({
+        method: 'put',
+        url: ROOT + destination,
+        data: {
+          content: contents.toString('base64'),
+          message: 'Upload via GitHub API: ' + destination,
+        },
+        headers,
+        maxBodyLength: Infinity,
+        maxContentLength: Infinity,
+      });
     })
+    .then(() => console.log(chalk.green(chalk.bold('Uploaded'), destination)))
+    .catch(error => {
+      const { response } = error;
+      if (response.data.message.includes(`"sha" wasn't supplied.`))
+        console.error(chalk.red(chalk.bold('File already exists:'), destination));
+      else
+        console.error(chalk.red(chalk.bold('Failed to put'), origin, 'at', destination));
+    });
 }
 
 async function main() {
@@ -27,12 +39,16 @@ async function main() {
     console.log('usage: node upload.js <inputPath> <contentPath>');
     return;
   }
+  const inputPath = process.argv[2];
+  const contentPath = process.argv[3];
 
-  const files = (await fs.readdir(process.argv[2]))
+  // Concurrency (plimit > 1) leads to a general, non-file-existing errors
+  const limit = pLimit(1);
+  const promises = (await fs.readdir(inputPath))
     .filter(fname => fname.includes('.json'))
-    .map(fname => input + '/' + fname);
+    .map(fname => limit(() => putFile(inputPath + '/' + fname, contentPath + '/' + fname)));
 
-  await putFile(files[0], process.argv[3]);
+  await Promise.all(promises);
 }
 
 main();
